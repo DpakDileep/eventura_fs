b47da89af60311db9e3056eef3cfffe05c3b67da
DpakDileep <dpakdileep003@gmail.com>
2025-12-06 12:47:47 +0530

    navbar component

--- eventura/src/App.jsx
+++ eventura/src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import Homepage from "./components/Homepage";
+import AppNavbar from "./components/AppNavbar";
 
 function App() {
   return (
     <>
+      <AppNavbar />
       <Homepage />
     </>
   );
---
+++ eventura/src/components/AppNavbar.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import {
+  Button,
+  Container,
+  FormControl,
+  InputGroup,
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavLink,
+} from "react-bootstrap";
+
+export default function AppNavbar() {
+  return (
+    <Navbar className="shadow">
+      <Container fluid className="mx-4 p-2">
+        <NavbarBrand>Eventura</NavbarBrand>
+        <InputGroup
+          className="border rounded-pill overflow-hidden ms-auto"
+          style={{ maxWidth: "750px" }}
+        >
+          <InputGroup.Text className="bg-light border-end">
+            <i className="bi bi-search"></i>
+          </InputGroup.Text>
+          <FormControl
+            className="btn btn-light"
+            placeholder="Search by event"
+          />
+          <FormControl
+            className="btn btn-light border-start"
+            placeholder="Search by location"
+          />
+          <InputGroup.Text className="bg-light border-end">
+            <i className="bi bi-geo-alt"></i>
+          </InputGroup.Text>
+        </InputGroup>
+        <Nav className="ms-auto">
+          <NavLink>My Tickets</NavLink>
+          <NavLink>Create Event</NavLink>
+          <Button variant="outline-dark rounded-pill ms-3">SignIn</Button>
+          <Button variant="outline-dark rounded-pill ms-3">SignUp</Button>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+}
