f445cb8db2347100fa29a86d0f017308c8d4fb91
DpakDileep <dpakdileep003@gmail.com>
2025-12-08 19:32:08 +0530

    homepage category ui done

--- eventura/src/components/EventsPage.jsx
+++ eventura/src/components/EventsPage.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
 import { Row, Col, Container } from "react-bootstrap";
 import EventCard from "./EventCard";
 
@@ -6,17 +7,21 @@ export default function EventsPage() {
   const [events, setEvents] = useState(
     JSON.parse(localStorage.getItem("events")) || []
   );
-  return(
-    <Container style={{paddingTop:"120px"}}>
-      <Row>
-        {events.map((event) => {
-          return (
-            <Col key={event.id} xs={12} sm={8} md={6} lg={3} className="mb-4"> 
-              <EventCard event={event} />
-            </Col>
-          );
-        })}
-      </Row>
-    </Container>
+  const location = useLocation();
+
+  return (
+    <>
+      <Container style={{ paddingTop: "120px" }}>
+        <Row>
+          {events.map((event) => {
+            return (
+              <Col key={event.id} xs={12} sm={8} md={6} lg={3} className="mb-4">
+                <EventCard event={event} />
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
+    </>
   );
 }
--- eventura/src/components/Homepage.jsx
+++ eventura/src/components/Homepage.jsx
@@ -1,8 +1,14 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import EventList from "./EventList";
-import { Button, Container } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import Banner from "../assets/images/banner2.png";
+import Music from "../assets/images/music.svg";
+import Dance from "../assets/images/dance.svg";
+import Gaming from "../assets/images/gaming.svg";
+import Comedy from "../assets/images/comedy.svg";
+import Party from "../assets/images/party.svg";
+import Tech from "../assets/images/tech.svg";
 import { Navigate, useNavigate } from "react-router-dom";
 
 export default function Homepage() {
@@ -51,6 +57,82 @@ export default function Homepage() {
         >
           Exploreb
         </Button>
+      </Container>
+      <Container className="mt-5">
+        <Row>
+          <Col md={2} className="text-center">
+            <img
+              src={Music}
+              alt="music"
+              className="img-fluid rounded-circle shadow-lg mb-2"
+              style={{ width: "100px", height: "100px", objectFit: "cover" }}
+              onClick={() =>
+                navigate("/events", { state: { category: "music" } })
+              }
+            />
+            <p>Music</p>
+          </Col>
+          <Col md={2} className="text-center">
+            <img
+              src={Dance}
+              alt="dance"
+              className="img-fluid rounded-circle shadow-lg mb-2"
+              style={{ width: "100px", height: "100px", objectFit: "cover" }}
+              onClick={() =>
+                navigate("/events", { state: { category: "dance" } })
+              }
+            />
+            <p>Dance</p>
+          </Col>
+          <Col md={2} className="text-center">
+            <img
+              src={Gaming}
+              alt="gaming"
+              className="img-fluid rounded-circle shadow-lg mb-2"
+              style={{ width: "100px", height: "100px", objectFit: "cover" }}
+              onClick={() =>
+                navigate("/events", { state: { category: "gaming" } })
+              }
+            />
+            <p>Gaming</p>
+          </Col>
+          <Col md={2} className="text-center">
+            <img
+              src={Comedy}
+              alt="comedy"
+              className="img-fluid rounded-circle shadow-lg mb-2"
+              style={{ width: "100px", height: "100px", objectFit: "cover" }}
+              onClick={() =>
+                navigate("/events", { state: { category: "comedy" } })
+              }
+            />
+            <p>Comedy</p>
+          </Col>
+          <Col md={2} className="text-center">
+            <img
+              src={Party}
+              alt="party"
+              className="img-fluid rounded-circle shadow-lg mb-2"
+              style={{ width: "100px", height: "100px", objectFit: "cover" }}
+              onClick={() =>
+                navigate("/events", { state: { category: "party" } })
+              }
+            />
+            <p>Party</p>
+          </Col>
+          <Col md={2} className="text-center">
+            <img
+              src={Tech}
+              alt="tech"
+              className="img-fluid rounded-circle shadow-lg mb-2"
+              style={{ width: "100px", height: "100px", objectFit: "cover" }}
+              onClick={() =>
+                navigate("/events", { state: { category: "tech" } })
+              }
+            />
+            <p>Tech</p>
+          </Col>
+        </Row>
       </Container>
       <Container className="mt-5">
         <EventList events={events.slice(0, visibleCard)} />
